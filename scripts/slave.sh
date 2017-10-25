8de5def25857e17a2048d21cf6c523b155bf8b3e
zouqilin <mr.zouqilin@gmail.com>
2017-10-26 01:10:16 +0800

    full backup before start pg service

--- scripts/slave.sh
+++ scripts/slave.sh
@@ -4,7 +4,7 @@ sed -i 's/^max_standby_streaming_delay.*$/max_standby_streaming_delay = 30s/g' $
 sed -i 's/^wal_receiver_status_interval.*$/wal_receiver_status_interval = 1s/g' ${PG_DATADIR}/postgresql.conf
 sed -i 's/^hot_standby_feedback.*$/hot_standby_feedback = on/g' ${PG_DATADIR}/postgresql.conf
 
-PGPASSWORD=${PG_PASSWORD} pg_basebackup -F p --progress -D ${PG_DATADIR} -h ${PG_MASTER_HOST} -p ${PG_MASTER_POST} -U ${PG_DUPLICATE_USER} -w -a
+# PGPASSWORD=${PG_PASSWORD} pg_basebackup -F p --progress -D ${PG_DATADIR} -h ${PG_MASTER_HOST} -p ${PG_MASTER_POST} -U ${PG_DUPLICATE_USER} -w -a
 
 cp ${PG_SHAREDIR}/recovery.conf.sample ${PG_DATADIR}/recovery.conf
 
@@ -13,9 +13,9 @@ sed -i "s/^recovery_target_timeline.*$/recovery_target_timeline = 'latest'/g" ${
 sed -i "s/^primary_conninfo.*$/primary_conninfo = 'host=${PG_MASTER_HOST} port=${PG_MASTER_POST} user=${PG_DUPLICATE_USER} password=${PG_DUPLICATE_PASSWORD}'/g" ${PG_DATADIR}/recovery.conf
 # sed -i "s/^sslmode.*$/sslmode = 'on'/g" ${PG_DATADIR}/recovery.conf
 
-# sshpass -p ${MASTER_ROOT_PASSWORD} rsync -cva --inplace --exclude=*pg_xlog* ${PG_MASTER_HOST}:${PG_DATADIR} ${PG_DATADIR}
+sshpass -p ${MASTER_ROOT_PASSWORD} rsync -cva --inplace --exclude=*pg_xlog* ${PG_MASTER_HOST}:${PG_DATADIR} ${PG_DATADIR}
 
 /etc/init.d/postgres restart
 
-# export cmd_ssh="sshpass -p ${MASTER_ROOT_PASSWORD} ssh -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null root@${PG_MASTER_HOST}"
-# ${cmd_ssh} /etc/init.d/postgresql restart
+export cmd_ssh="sshpass -p ${MASTER_ROOT_PASSWORD} ssh -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null root@${PG_MASTER_HOST}"
+${cmd_ssh} /etc/init.d/postgresql restart
